d103f56bfb509124ecb0018b4c9f37fb732630fd
Manjushree-scu <MKrishnamurthy@scu.edu>
2022-09-25 07:27:16 -0700

    Update test.py
    Minor changes to go over the complete company.txt file

--- code-lab1/test.py
+++ code-lab1/test.py
@@ -2,13 +2,19 @@ import re
 
 file1 = open('company.txt', 'r')
 Lines = file1.readlines()
-line = Lines[1]
+count = 0
+for line in Lines:
+    x = re.search(r"\w+\sINC", line)
+    if x!=None:
+        break
+    count+=1
 
-x = re.search(r"\w+\s\w+", line)
-
-file2 = open("company.txt", "w")
-file2.write(x.group())
-file2.close()
-print("Printing File content")
-f = open("company.txt", "r")
-print(f.read())
+if x != None :
+    file2 = open("company.txt", "w")
+    file2.write(x.group())
+    file2.close()
+    print("Printing File content")
+    f = open("company.txt", "r")
+    print(f.read())
+else:
+    print("No Company name found")
